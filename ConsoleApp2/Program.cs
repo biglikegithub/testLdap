b8deb85087ca13e9b0f63255df61ea10140b691d
munguu <noyokhon@gmail.com>
2023-12-25 17:40:23 +0800

    init

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.DirectoryServices;
+using System.Reflection.PortableExecutable;
+
+Console.WriteLine("Hello, World!");
+
+
+bool re;
+try
+{
+    System.DirectoryServices.DirectoryEntry de = new("LDAP://server.mn", "", "");
+    DirectorySearcher dSearch = new(de);
+    SearchResult results = dSearch.FindOne();
+    re = true;
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+    re = false;
+
+}
+Console.WriteLine(re) ;
